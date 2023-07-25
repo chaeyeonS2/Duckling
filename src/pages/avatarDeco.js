5d7b8088531dafc11ed25910a24f1a5121a1c1b9
chaeyeon <321codus@naver.com>
2023-07-26 00:12:35 +0200

    아바타 얼굴 확대, 썸네일 추가
    - 꾸미기 얼굴 선택 시 아바타 얼굴 확대하기
    - 아이템 인벤토리에 PNG 썸네일 넣기

--- src/pages/avatarDeco.js
+++ src/pages/avatarDeco.js
@@ -16,21 +16,51 @@ const NewMapModel = (props) => {
         <pointLight intensity={1}/>
         <primitive 
             object={scene}
-            scale={1.2}
-            position={[0,-0.07,0]}
+            scale={1.5}
+            position={[0,-0.08,0]}
         />
     </mesh>
      
     );
 };
 
-// const SelectType = () => {
-//     const [typeState, setTypeState] = useState("eye");
-//     typeBtn.current.style.backgroundColor = '#BDFF6B';
-//     return console.log("it work!")
-// }
+const ItemModel = (props) => {
+    const { scene } = useGLTF(process.env.PUBLIC_URL  +'/img/hani_avatar_netural.gltf');
+    
+    return (
+    <mesh>
+        <pointLight intensity={1}/>
+        <primitive 
+            object={scene}
+            scale={1.2}
+            position={[0,-0.1,0]}
+        />
+    </mesh>
+     
+    );
+};
 
+const Square = () => {
+    return (
+      <mesh rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25}>
+        <boxGeometry args={[0.05,0.05,0.05]}/>
+        <meshStandardMaterial color={"blue"} />
+      </mesh>
+    )
+  }
+
+
+  
 const AvatarDeco = () => {
+    var imgSrcList = [process.env.PUBLIC_URL + "/img/VectorsmileTrue.png",
+                    process.env.PUBLIC_URL + "/img/VectorsmileFalse.png",
+                    process.env.PUBLIC_URL + "/img/VectorclothTrue.png",
+                    process.env.PUBLIC_URL + "/img/VectorclothFalse.png"]
+    const [imgSrc, setImageSrc] = useState(imgSrcList[0]);
+    const [typeDecoState, setDecoTypeState] = useState();  //데코(얼굴, 옷) 카테고리
+    const [isClicked, setIsClicked] = useState(false);
+    
+    const [typeItemState, setItemTypeState] = useState("eye");  //아이템 카테고리
     var itemArray = [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12]; //item 개수
     return (
         <div className="layoutDeco">
@@ -46,13 +76,15 @@ const AvatarDeco = () => {
                 
                 <div className="faceDeco">
                     <div className="faceBtnGroup">
-                        <div className="face_btn eye">눈</div>
-                        <div className="face_btn mouth">입</div>
+                        <div className={typeItemState === "eye" ? "selectBtn" : "face_btn"} onClick={() => {setItemTypeState("eye")}}>눈</div>
+                        <div className={typeItemState === "mouth" ? "selectBtn" : "face_btn"} onClick={() => {setItemTypeState("mouth")}}>입</div>
                     </div>
                     <div>
                         {
                         itemArray.map(function(){
-                            return (<div className="item_box"></div>)
+                            return (<div className="item_box">
+                                <img className="item_img" src={process.env.PUBLIC_URL + "/img/jean.png"} img alt="my image"/>
+                            </div>)
                         })
                         };  
                     </div>
@@ -61,8 +93,8 @@ const AvatarDeco = () => {
                 
             </div>
             <div className="chooseBtnGroup">
-                <div><button id="btn_face"><img src={process.env.PUBLIC_URL + "/img/VectorsmileTrue.png"} img alt="my image"/></button></div>
-                <div><button id="btn_cloth"><img src={process.env.PUBLIC_URL + "/img/VectorclothTrue.png"} img alt="my image"/></button></div>
+                <div className="btn_face" onClick={()=>{setImageSrc(imgSrcList[0])}}><img src={imgSrc} img alt="my image"/></div>
+                <div className="btn_cloth"onClick={()=>{setImageSrc(imgSrcList[3])}}><img src={imgSrc} img alt="my image"/></div>
             </div>
             
         </div>
