127893257bd02044ff7c7e4bcac235c33bd730e1
chaeyeon <321codus@naver.com>
2023-08-17 04:30:42 +0900

    서버 연동 test

--- src/pages/tmp.js
+++ src/pages/tmp.js
@@ -1,174 +1,38 @@
-import React, { useState, useRef, useEffect, useLayoutEffect } from "react";
-import { BottomSheet } from 'react-spring-bottom-sheet';
-// import 'react-spring-bottom-sheet/dist/style.css'; // 스타일 파일을 불러옵니다.
-import "../../css/customBottomSheet_postView.css";
+import React, {Fragment, useState} from 'react';
+import axios from 'axios';
 
-const CommentView = () => {
-    const [isCommentOpen, setIsCommentOpen] = useState(false);
-    const [show, setShow] = useState(false); // show 상태와 setShow 함수를 선언합니다.
-
-    const print = () => {  
-        console.log("touch"); 
-    } 
-    const sheetRef = useRef() 
-    
-    const [comment, setComment] = useState('');
-
-    const handleCommentChange = (event) => {
-      setComment(event.target.value);
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      // 여기에 댓글을 서버에 보내는 로직을 추가할 수 있습니다.
-      console.log('댓글 제출:', comment);
-      setComment(''); // 댓글 입력란 초기화
-    };
-
-    
-
-    const modalRef = useRef(null);
-    const [portalCreated, setPortalCreated] = useState(false);
-     
-    useLayoutEffect(() => {
-        // BottomSheet가 항상 열려있는 상태에서 실행될 코드
-        const parentDiv = document.getElementById('parentDiv-post');
-        if (parentDiv) {
-          const childDivs = parentDiv.querySelectorAll('div');
-          
-          childDivs.forEach((childDiv) => {
-            childDiv.classList.add('postSheet');
-          });
-          
-          return () => {
-            childDivs.forEach((childDiv) => {
-              childDiv.classList.remove('postSheet');
-            });
-          };
-        }
-      }, []);
-    
- 
-
+const Tmp = () => {
+    const [data, setData] = useState(null);
+    const onClick = ()=>{
+        axios.get('https://us-central1-netural-app.cloudfunctions.net/api/assets/top_1').then(reponse => {
+        setData(reponse.data);
+      });
+    }
+    // const item = data.assetImg._path.segments[3];
     return(
-        <BottomSheet className="postSheet" id="parentDiv-post"
-            open // 바닥 시트 열림 상태
-            //onDismiss={() => setShow(false)} // 닫기 버튼 클릭 시 호출되는 함수
-            //snapPoints={({ maxHeight }) => {snapPoints}}
-            scrollLocking = {false}
-            snapPoints={({ maxHeight }) => [
-                maxHeight/19, //최소
-                maxHeight /2, //최대
-                ]}
-                ref={sheetRef}
-                header={
-                <div className="bottomBar">
-                    <div className='candy'>
-                        <div><img src={process.env.PUBLIC_URL + "/img/writing/cookie.png"}/></div>
-                        <div className='num'>215</div>
-                    </div>
-                    <div className='comment' onClick={() => 
-                        sheetRef.current.snapTo(({ snapPoints }) => Math.max(...snapPoints), {
-                            // Each property is optional, here showing their default values
-                            source: 'snap-to-top',
-                            velocity: 1,
-                            })}>
-                        <div><img src={process.env.PUBLIC_URL + "/img/writing/comment.png"}/></div>
-                        <div className='num'>423</div>
-                    </div>
-
-                    <button onClick={() => sheetRef.current.snapTo(0, { source: 'snap-to-bottom' })}>close</button>
-                
-
-                </div>
-                }
-            > 
-            <div className="BottomSheet-content">
-               <div className="comment-get-layout">
-                <div className="commentBox">
-                        <div className='commentTop'>
-                            <div className="profileImg">
-                                {/* 서버에서 받아온 이미지 넣기 */}
-                            </div> 
-                            <div className="userName">
-                                팜하니
-                            </div>
-                        </div>
-                        <div className='commentContent'>
-                        너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무
-
-                        </div>
-                    </div>
-                <div className="commentBox">
-                        <div className='commentTop'>
-                            <div className="profileImg">
-                                {/* 서버에서 받아온 이미지 넣기 */}
-                            </div>
-                            <div className="userName">
-                                팜하니
-                            </div>
-                        </div>
-                        <div className='commentContent'>
-                        너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무
-
-                        </div>
-                    </div>
-                    <div className="commentBox">
-                        <div className='commentTop'>
-                            <div className="profileImg">
-                                {/* 서버에서 받아온 이미지 넣기 */}
-                            </div> 
-                            <div className="userName">
-                                팜하니
-                            </div>
-                        </div>
-                        <div className='commentContent'>
-                        너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무
-
-                        </div>
-                    </div>
-                    <div className="commentBox">
-                        <div className='commentTop'>
-                            <div className="profileImg">
-                                {/* 서버에서 받아온 이미지 넣기 */}
-                            </div>
-                            <div className="userName">
-                                팜하니
-                            </div>
-                        </div>
-                        <div className='commentContent'>
-                        너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무 재미있어요. super shy~~~너무
-                        </div>
-                    </div>
-            
-                </div> 
-            
-                <div className="writeComment">
-                    <div >
-                        <form onSubmit={handleSubmit} className="commentInput">
-                            <textarea
-                            rows="3"
-                            placeholder="댓글을 입력하세요..."
-                            value={comment}
-                            onChange={handleCommentChange}
-                            ></textarea>
-                            <br />
-
-                            <button className="Btn_comment" type="submit">
-                            <img src={process.env.PUBLIC_URL + "/img/writing/comment-upload-true-btn.png"}/>
-                            </button>
-                        </form>
-
-                </div>
-
-                </div>
-            
-            
- 
-            </div>
-        </BottomSheet>
-
-    )
+        <div>
+        <div>
+          <h3>Axios 테스트해보기</h3>
+          <button onClick={onClick}>불러오기</button>
+        </div>
+        {data && 
+          <div>
+            <br />
+            Gltf : {data.assetGltf} <br/>  
+            Image: {data.assetImg} <br/> 
+            Kind : {data.kind} <br /> 
+          </div>
+        }
+        {/* {data.assetImg} */}
+        {data && data.assetImg && <img src={data.assetImg} />}
+      </div>
+    );
 }
 
-export default CommentView
+export default Tmp
+
+// {
+//     "kind": "top",
+//     "assetGltf": "https://firebasestorage.googleapis.com/v0/b/netural-app.appspot.com/o/tops_gltf%2Fhani_top.gltf?alt=media&token=448e7601-2711-447d-8545-9bf1ba941bca",
+//     "assetImg": "https://firebasestorage.googleapis.com/v0/b/netural-app.appspot.com/o/tops_png%2Fhani_top.png?alt=media&token=469e3555-9171-4ff4-8ba8-9591c0ef10e5"
+// }
