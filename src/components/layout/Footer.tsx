9f5ebc884bbecd1c7a602e0892ab82dc9ea8c572
Sharlotte <aaa9810321@gmail.com>
2023-10-10 15:31:05 +0900

    refactor: 접근성 향상 - `navigate` 대신 Link 사용

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,26 +1,14 @@
 import * as styles from "./footer.css";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export interface FooterProps {
   btn?: number;
 }
 export default function Footer({ btn: active }: FooterProps) {
-  // TODO: just use a tag
-  const navigate = useNavigate();
-  const homeClick = () => {
-    navigate("/home");
-  };
-  const lookClick = () => {
-    navigate("/look");
-  };
-  const cameraClick = () => {
-    navigate("/camera");
-  };
-
   //어떤 버튼이 활성화인지
-  var homeImg = "/img/home.png";
-  var avatarImg = "/img/person.png";
-  var cameraImg = "/img/camera.png";
+  let homeImg = "/img/home.png";
+  let avatarImg = "/img/person.png";
+  let cameraImg = "/img/camera.png";
 
   // TODO: tab ui같은데 index를 내부적으로 제어하도록 만들기
   const btnActive = () => {
@@ -39,15 +27,15 @@ export default function Footer({ btn: active }: FooterProps) {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerBtnGroup}>
-        <button className={styles.footerBtn} onClick={lookClick}>
+        <Link to="/home" className={styles.footerBtn}>
           <img src={homeImg} />
-        </button>
-        <button className={styles.footerBtn} onClick={homeClick}>
+        </Link>
+        <Link to="/look" className={styles.footerBtn}>
           <img src={avatarImg} />
-        </button>
-        <button className={styles.footerBtn} onClick={cameraClick}>
+        </Link>
+        <Link to="/camera" className={styles.footerBtn}>
           <img src={cameraImg} />
-        </button>
+        </Link>
       </div>
     </footer>
   );
