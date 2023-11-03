4f903b9e2a7d571c0f62ad57c6252a685c31fdb4
chaeyeon <321codus@naver.com>
2023-11-04 00:20:23 +0900

    feat: 바텀 시트 재제작 완료

--- src/pages/home/_components/MyPost.tsx
+++ src/pages/home/_components/MyPost.tsx
@@ -1,43 +1,88 @@
 //https://github.com/Temzasse/react-modal-sheet#vanilla-css
-import Sheet from "react-modal-sheet";
-import { useState } from "react";
-import style from "styled-components";
-
-const CustomSheet = style(Sheet)`
-  margin: 0 auto;
-  max-width: 680px;
-
-  .react-modal-sheet-container {
-    background-color: #222 !important;
-  }
-
-  .react-modal-sheet-backdrop {
-    background-color: rgba(0, 0, 0, 0.3) !important;
-  }
-
-  .react-modal-sheet-drag-indicator {
-    background-color: #666 !important;
-  }
-`;
+import Sheet, { SheetRef } from "react-modal-sheet";
+import { useRef } from "react";
+import * as styles from "./myPost.css";
+import { useNavigate } from "react-router-dom";
+import useSWR from "swr";
 
 export default function MyPost() {
-  const [isOpen, setOpen] = useState(false);
+  const ref = useRef<SheetRef>();
+  const snapTo = (i: number) => ref.current?.snapTo(i);
 
   return (
     <>
-      <div>
-        <button onClick={() => setOpen(true)}>Open sheet</button>
-      </div>
-
-      <Sheet isOpen={true} onClose={() => setOpen(true)}>
-        <Sheet.Container>
-          <Sheet.Header />
+      <Sheet
+        isOpen={true}
+        onClose={() => snapTo(1)}
+        snapPoints={[500, 80]}
+        initialSnap={1}
+        style={{
+          marginBottom: "88px",
+          borderLeft: "1px solid black",
+          borderRight: "1px solid black",
+        }}
+      >
+        <Sheet.Container
+          style={{
+            borderTopRightRadius: "20px",
+            borderTopLeftRadius: "20px",
+          }}
+        >
+          <Sheet.Header
+            style={{
+              height: "55px",
+              borderRadius: "20px 20px 0 0",
+              borderTop: "5px solid black",
+              boxShadow: "0px -2px 0px rgba(0, 0, 0, 0.3)",
+              paddingTop: "12px",
+              paddingBottom: "12px",
+            }}
+          >
+            <CustomHeader />
+          </Sheet.Header>
           <Sheet.Content>
-            <div>aaa</div>
+            <Sheet.Scroller draggableAt="both">
+              <CustomContent />
+            </Sheet.Scroller>
           </Sheet.Content>
         </Sheet.Container>
-        <Sheet.Backdrop />
+        <Sheet.Backdrop style={{ backgroundColor: "rgba(0,0,0,0)" }} />
       </Sheet>
     </>
+  );
+}
+
+function CustomHeader() {
+  const navigate = useNavigate();
+  const newpostClick = () => {
+    navigate("/newPost");
+  };
+  return (
+    <div>
+      <div className={styles.profileImg} style={{ backgroundImage: `url(${localStorage.getItem("photoURL")})` }} />
+      <div className={styles.userName}>{localStorage.getItem("userName")}</div>
+      <div className={styles.btnAddNew} onClick={newpostClick}>
+        <img className={styles.btnAddNewImage} src="/img/writing/add.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+function CustomContent() {
+  const { data: posts } = useSWR<APIPostsWriterReponse>("/api/posts/writer/" + localStorage.getItem("userName"));
+
+  const navigate = useNavigate();
+  const handlePostClick = (userName: string, postID: string) => {
+    navigate(`/postview/${userName}/${postID}`);
+  };
+  return (
+    <div className={styles.content}>
+      {posts &&
+        posts.map((post, index) => (
+          <div key={index} className={styles.postImg} onClick={() => handlePostClick(post.writerID, post.postID)}>
+            <img className={styles.item_img} src={post.postImg[0]} alt="" />
+          </div>
+        ))}
+    </div>
   );
 }
