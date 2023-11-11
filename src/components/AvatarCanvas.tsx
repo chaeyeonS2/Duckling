9c36f1389f0c6b96542dee79cc36b971a0afb933
Sharlotte <aaa9810321@gmail.com>
2023-11-12 04:07:54 +0900

    fix: three.js webglrenderer context list

--- src/components/AvatarCanvas.tsx
+++ src/components/AvatarCanvas.tsx
@@ -1,42 +1,54 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas, RootState } from "@react-three/fiber";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useCallback } from "react";
 
-const AvatarCanvas = forwardRef<
-  RootState,
-  React.PropsWithChildren<React.ComponentProps<typeof Canvas> & { canvasRef?: React.Ref<HTMLCanvasElement> }>
->(({ children, canvasRef, ...props }, ref) => {
-  return (
-    <Canvas
-      ref={canvasRef}
-      onCreated={(rootState) => {
-        if (!ref) return;
-        if (typeof ref === "function") ref(rootState);
-        else ref.current = rootState;
-      }}
-      style={{ background: "transparent", position: "absolute", width: "100%", height: "100%" }}
-      shadows
-      camera={{
-        rotation: [0, 0, 0],
-        fov: 150,
-        zoom: 100,
-        near: 1,
-        far: 10,
-      }}
-      {...props}
-    >
-      <spotLight position={[0, 0.2, 0.5]} />
-      <ambientLight />
+const AvatarCanvas = forwardRef<RootState, React.PropsWithChildren<React.ComponentProps<typeof Canvas>>>(
+  ({ children, ...props }, ref) => {
+    const rootStateRef = useCallback((rootState: RootState) => {
+      if (!ref) return;
 
-      {children}
+      rootState.gl.domElement.addEventListener(
+        "webglcontextlost",
+        function (event) {
+          event.preventDefault();
+          setTimeout(function () {
+            rootState.gl.forceContextRestore();
+          }, 100);
+        },
+        false
+      );
 
-      <OrbitControls
-        enableRotate // 회전 활성화
-        enablePan={false} // 이동 비활성화
-        enableDamping // 부드러운 움직임 효과 활성화
-        dampingFactor={0.1} // 부드러운 움직임 강도 설정
-      />
-    </Canvas>
-  );
-});
+      if (typeof ref === "function") ref(rootState);
+      else ref.current = rootState;
+    }, []);
+
+    return (
+      <Canvas
+        onCreated={rootStateRef}
+        style={{ background: "transparent", position: "absolute", width: "100%", height: "100%" }}
+        shadows
+        camera={{
+          rotation: [0, 0, 0],
+          fov: 150,
+          zoom: 100,
+          near: 1,
+          far: 10,
+        }}
+        {...props}
+      >
+        <spotLight position={[0, 0.2, 0.5]} />
+        <ambientLight />
+
+        {children}
+
+        <OrbitControls
+          enableRotate // 회전 활성화
+          enablePan={false} // 이동 비활성화
+          enableDamping // 부드러운 움직임 효과 활성화
+          dampingFactor={0.1} // 부드러운 움직임 강도 설정
+        />
+      </Canvas>
+    );
+  }
+);
 export default AvatarCanvas;
--- src/components/GroupWrapper.tsx
+++ src/components/GroupWrapper.tsx
@@ -1,18 +1,16 @@
 import { GroupProps } from "@react-three/fiber";
-import { useRef } from "react";
 
 export interface GroupWrpperProps extends GroupProps {
   groups: THREE.Group[];
 }
 function GroupWrpper({ groups, ...props }: GroupWrpperProps) {
-  const groupRef = useRef<THREE.Group>(null);
-
-  if (groupRef.current && groups.length > 0) {
-    groupRef.current.clear();
-    groupRef.current.add(...groups);
-  }
-
-  return <group ref={groupRef} {...props} />;
+  return (
+    <group {...props}>
+      {groups.map((group) => (
+        <primitive key={group.id} object={group} />
+      ))}
+    </group>
+  );
 }
 
 export default GroupWrpper;
