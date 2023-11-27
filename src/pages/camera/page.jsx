249e1d8c4ba20cb1e6cef9a116acde29fa307142
chaeyeon <321codus@naver.com>
2023-11-28 00:11:05 +0900

    chore: change scale

--- src/pages/camera/page.jsx
+++ src/pages/camera/page.jsx
@@ -1,4 +1,5 @@
 import View3D, { ARButton, AROverlay } from "@egjs/react-view3d";
+import ARScaleControl from "@egjs/react-view3d";
 //import * as styles from "./page.css";
 import "./page.css";
 import { useEffect, useRef } from "react";
@@ -7,18 +8,34 @@ import "@egjs/view3d/css/view3d-bundle.min.css";
 import "@egjs/view3d/css/view3d-ar.min.css";
 
 export default function ARCameraPage() {
-  const view3DRef = useRef(null);
+  const view3DRef = useRef();
+  const arScaleControlOptions = {
+    min: 1,
+    max: 1,
+  };
+
+  const arButtonOptions = {
+    availableText: "View in AR",
+    unavailableText: "AR is not available in this browser",
+    // 나머지 옵션들도 필요한 경우에 수정해주세요.
+  };
+
+  useEffect(() => {
+    view3DRef.current.loadPlugins(new ARButton(arButtonOptions));
+    const arScaleControl = new ARScaleControl(arScaleControlOptions);
+    view3DRef.current.arScaleControl;
+  }, []);
 
   return (
     <>
       <View3D
         ref={view3DRef}
         src="/gltf/test/animation_100.glb"
-        iosSrc="/gltf/test/test3.usdz"
+        iosSrc="/gltf/test/test03_3.usd"
         arPriority={["webAR", "sceneViewer", "quickLook"]}
         initialZoom={5}
         center={[0.2, 1, 0]}
-        //loadPlugins={[new ARButton()]}
+        //loadPlugins={arBtn}
       ></View3D>
       <button
         onClick={() => {
