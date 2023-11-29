2493b1d6a82e8e2d986d403461664b67e77abc23
Sharlotte <aaa9810321@gmail.com>
2023-11-30 06:39:00 +0900

    fix: postview page에서 data fetching 시 footer가 잠시 올라오는 문제

--- src/pages/postview/page.tsx
+++ src/pages/postview/page.tsx
@@ -10,7 +10,7 @@ import * as styles from "./page.css";
 
 export default function PostViewPage() {
   const { writerID, postID } = useParams();
-  const { data: postData } = useSWRImmutable(`/api/posts/writer/${writerID}/${postID}`);
+  const { data: postData } = useSWRImmutable<APIPostResponse>(`/api/posts/writer/${writerID}/${postID}`);
 
   const navigate = useNavigate();
   const commentClick = () => {
@@ -20,29 +20,27 @@ export default function PostViewPage() {
   return (
     <div className={styles.layout}>
       <HeaderPostView />
-      {postData && (
-        <div className={styles.container}>
-          <div className={styles.postHeader}>
-            <div>
-              <p className={styles.title}>{postData.title}</p>
-              <p className={styles.timestemp}>{postData.date}</p>
-            </div>
-            <Icon id="vertical-dots" size="medium" />
+      <div className={styles.container}>
+        <div className={styles.postHeader}>
+          <div>
+            <p className={styles.title}>{postData?.title}</p>
+            <p className={styles.timestemp}>{postData?.date}</p>
           </div>
-          <ImageSlider images={postData.postImg} />
-          <p className={styles.content}>{postData.body}</p>
-          <div className={styles.metadataContainer}>
-            <div className={styles.metadata}>
-              <Icon id="cookie" size="medium" />
-              <span>{postData.likes}</span>
-            </div>
-            <div onClick={commentClick} className={styles.metadata}>
-              <Icon id="comment" size="medium" />
-              <span>{postData.commentCount}</span>
-            </div>
+          <Icon id="vertical-dots" size="medium" />
+        </div>
+        <ImageSlider images={postData?.postImg ?? []} />
+        <p className={styles.content}>{postData?.body}</p>
+        <div className={styles.metadataContainer}>
+          <div className={styles.metadata}>
+            <Icon id="cookie" size="medium" />
+            <span>{postData?.likes}</span>
+          </div>
+          <div onClick={commentClick} className={styles.metadata}>
+            <Icon id="comment" size="medium" />
+            <span>{postData?.commentCount}</span>
           </div>
         </div>
-      )}
+      </div>
       <Footer />
     </div>
   );
