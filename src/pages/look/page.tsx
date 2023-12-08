a7bafc195f8af48287a4171e32841766362d3492
Sharlotte <aaa9810321@gmail.com>
2023-12-09 08:19:52 +0900

    feat: 게시물 인기순 정렬

--- src/pages/look/page.tsx
+++ src/pages/look/page.tsx
@@ -1,19 +1,24 @@
 import Footer from "@/components/layout/Footer";
 import PostMetadataBar from "@/components/PostMetadataBar";
-import HeaderLook from "@/components/layout/headers/HeaderLook";
 
 import useSWRInfinite from "swr/infinite";
 import useSWRImmutable from "swr/immutable";
 import { useNavigate } from "react-router-dom";
 
 import * as styles from "./page.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Icon from "@/components/Icon";
 
 const PAGE_SIZE = 5;
 export default function LookPage() {
   const navigate = useNavigate();
+  const [currentTab, setCurrentTab] = useState<"최신" | "인기">("최신");
+  const handleTabChange = (tab: "최신" | "인기") => () => {
+    setCurrentTab(tab);
+    setSize(1);
+  };
   const { data, size, setSize, isValidating, isLoading } = useSWRInfinite<APIPostsResponse>(
-    (index) => `/api/posts/?limit=${PAGE_SIZE}&start=${index * PAGE_SIZE}`
+    (index) => `/api/posts/${currentTab == "최신" ? "" : "likes/"}?limit=${PAGE_SIZE}&start=${index * PAGE_SIZE}`
   );
 
   const handlePostClick = (userName: string, postID: string) => () => {
@@ -43,7 +48,16 @@ export default function LookPage() {
 
   return (
     <div className={styles.layout}>
-      <HeaderLook />
+      <header className={styles.header}>
+        <div className={styles.tab} onClick={handleTabChange("인기")} aria-selected={currentTab === "인기"}>
+          <Icon id="finger-heart" size="medium" />
+          <p className={styles.tabText}>인기</p>
+        </div>
+        <div className={styles.tab} onClick={handleTabChange("최신")} aria-selected={currentTab === "최신"}>
+          <Icon id="finger-rock" size="medium" />
+          <p className={styles.tabText}>최신</p>
+        </div>
+      </header>
       <div className={styles.content} ref={contentElemRef}>
         {posts?.map((post, index) => (
           <div className={styles.postBox} key={index} onClick={handlePostClick(post.writerID, post.postID)}>
