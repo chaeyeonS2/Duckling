13d8697373dd4f51748b33f6a543f92579aac424
Sharlotte <aaa9810321@gmail.com>
2023-12-20 13:54:11 +0900

    prov: share page avatar y coord

--- src/pages/share/page.tsx
+++ src/pages/share/page.tsx
@@ -63,7 +63,7 @@ export default function SharePage() {
       <div className={styles.pageContainer}>
         <div className={styles.canvasContainer}>
           <AvatarCanvas ref={rootStateRef}>
-            <AvatarModelGroup userId={userID} position={[0, -0.08, 0]} />
+            <AvatarModelGroup userId={userID} position={[0, -0.05, 0]} />
           </AvatarCanvas>
         </div>
         <button onClick={onCaptureClick} className={button + " " + styles.captureButton} aria-selected>
