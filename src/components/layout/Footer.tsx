aa07d59b686e2963431ed21182179d18f8162706
Sharlotte <aaa9810321@gmail.com>
2024-02-12 22:52:04 +0900

    asdf

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Link to="/look" className={styles.footerBtn} aria-checked={tabIdx[0].includes(pathname)}>
-        <DynamicIcon className={styles.icon} id={`home${tabIdx[0].includes(pathname) ? "-fill" : ""}`} />
+        <DynamicIcon size="large" className={styles.icon} id={`home${tabIdx[0].includes(pathname) ? "-fill" : ""}`} />
       </Link>
       <Link to="/home" className={styles.footerBtn} aria-checked={tabIdx[1].includes(pathname)}>
-        <DynamicIcon className={styles.icon} id={`mypage${tabIdx[1].includes(pathname) ? "-fill" : ""}`} />
+        <DynamicIcon size="large" className={styles.icon} id={`mypage${tabIdx[1].includes(pathname) ? "-fill" : ""}`} />
       </Link>
       <Link to="/camera" className={styles.footerBtn} aria-checked={tabIdx[2].includes(pathname)}>
-        <DynamicIcon className={styles.icon} id={`camera${tabIdx[2].includes(pathname) ? "-fill" : ""}`} />
+        <DynamicIcon size="large" className={styles.icon} id={`camera${tabIdx[2].includes(pathname) ? "-fill" : ""}`} />
       </Link>
     </footer>
   );
