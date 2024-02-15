72e37c02e8d9835fdd54d16c3517a912153f99af
Sharlotte <aaa9810321@gmail.com>
2024-02-16 03:37:47 +0900

    fix: 카메라 페이지 로드 실패

--- src/pages/camera/page.tsx
+++ src/pages/camera/page.tsx
@@ -1,66 +1,92 @@
-import View3D from "@egjs/react-view3d";
-import * as styles from "./page.css";
 import { useRef, useState } from "react";
-import Header from "@/components/layout/headers/Header";
-import { Link } from "react-router-dom";
 
-import "@egjs/view3d/css/view3d-bundle.min.css";
-import "@egjs/view3d/css/view3d-ar.min.css";
+import useSWRImmutable from "swr/immutable";
+
+import View3D from "@egjs/react-view3d";
+import { Link } from "react-router-dom";
+import Flicking from "@egjs/react-flicking";
 import Footer from "@/components/layout/Footer";
-import AnimationMenu from "./_components/AnimationMenu";
 import { DynamicIcon } from "@/components/Icon";
+import Header from "@/components/layout/headers/Header";
+
+import * as styles from "./page.css";
+import "@egjs/view3d/css/view3d-ar.min.css";
+import "@egjs/view3d/css/view3d-bundle.min.css";
 
 export default function ARCameraPage() {
-  const view3DRef = useRef<View3D | null>(null);
-  const [playingGtlfModel, setplayingGtlfModel] = useState<{ gltfPath: string; assetID: string } | null>(null);
+  const view3DRef = useRef<View3D>(null);
+  const [motionAsset, setMotionAsset] = useState<Asset>();
 
-  const usdzDataset: { [key: string]: string } = {
-    "2784869c-eb65-4e77-945a-3486e9b4f0e8": "usdz/Danniel_Fin.usdz", //danniel
-    "43bf33ce-d1ad-4232-86b6-46a67e2ee532": "usdz/Haerin_Fin.usdz", //haerin
-    "947b3e1c-40db-453f-a987-d0f29618c1b2": "usdz/Hanni_Fin.usdz", //hanni
-    "e67207f2-9a91-4041-ad86-e8bb2cf51bf1": "usdz/hyein.usdz", //hyein
-    "877605f7-8b15-4020-80b5-28639136f82f": "usdz/minji.usdz", //minji
+  const { data: icons } = useSWRImmutable(`/api/assets/?kind=ar`);
+
+  const handleMotionChange = (motion: Asset) => async () => {
+    if (!view3DRef.current) return;
+    setMotionAsset(motion);
+    await view3DRef.current.load(motion.assetGltf, { iosSrc: motion.assetUsdz });
+    if (!view3DRef.current.initialized) {
+      view3DRef.current.init();
+    }
   };
 
-  const gltfDataset: { [key: string]: string } = {
-    "2784869c-eb65-4e77-945a-3486e9b4f0e8": "gltf/avatar/Danniel_Fin.gltf", //danniel
-    "43bf33ce-d1ad-4232-86b6-46a67e2ee532": "gltf/avatar/Haerin_Fin.gltf", //haerin
-    "947b3e1c-40db-453f-a987-d0f29618c1b2": "gltf/avatar/Hanni_Fin.gltf", //hanni
-    "e67207f2-9a91-4041-ad86-e8bb2cf51bf1": "gltf/avatar/hyein.glb", //hyein
-    "877605f7-8b15-4020-80b5-28639136f82f": "gltf/avatar/minji.glb", //minji
+  const enterAR = async () => {
+    if (!view3DRef.current) return;
+    const arAvailable = await view3DRef.current.ar.isAvailable();
+    console.log("is available:", arAvailable);
+    console.log("current motion:", motionAsset);
+    console.log("current session:", view3DRef.current.ar.activeSession);
+    await view3DRef.current.ar.enter();
   };
-
-  const handleDataFromAnimationMenu = (gltfPath: string, assetID: string) => {
-    setplayingGtlfModel({ gltfPath, assetID });
-  };
+  const isValid = !!motionAsset;
 
   return (
-    <>
-      <div className={styles.pageContainer}>
-        <Header closable>
-          <Link to={"/share/" + localStorage.getItem("id")}>
-            <DynamicIcon id="share" size="medium" />
-          </Link>
-          <Link to="/deco">
-            <DynamicIcon id="clothes" size="medium" />
-          </Link>
-          <Link to="/setting">
-            <DynamicIcon id="settings" size="medium" />
-          </Link>
-        </Header>
-        <View3D
-          key={playingGtlfModel?.assetID}
-          className={styles.canvas}
-          ref={view3DRef}
-          src={playingGtlfModel ? gltfDataset[playingGtlfModel.assetID] : ""}
-          iosSrc={playingGtlfModel ? usdzDataset[playingGtlfModel.assetID] : ""}
-          arPriority={["webAR", "sceneViewer", "quickLook"]}
-          center={[0, 0.9, 0.25]} //위치 조정
-          useDefaultEnv={true}
-        ></View3D>
-        <AnimationMenu ref={view3DRef} animationGltf={handleDataFromAnimationMenu} />
-        <Footer />
+    <div className={styles.pageContainer}>
+      <Header closable>
+        <Link to={"/share/" + localStorage.getItem("id")}>
+          <DynamicIcon id="share" size="medium" />
+        </Link>
+        <Link to="/deco">
+          <DynamicIcon id="clothes" size="medium" />
+        </Link>
+        <Link to="/setting">
+          <DynamicIcon id="settings" size="medium" />
+        </Link>
+      </Header>
+      <View3D
+        className={styles.canvas}
+        ref={view3DRef}
+        arPriority={["webAR", "sceneViewer", "quickLook"]}
+        center={[0, 0.9, 0.25]} //위치 조정
+        autoInit={false}
+        useDefaultEnv
+      />
+      <div className={styles.menuContainer}>
+        <p className={styles.title}>아바타 모션</p>
+        <div className={styles.iconContainer}>
+          <button className={styles.camera} disabled={!isValid} onClick={enterAR}>
+            <DynamicIcon id="avatarmotion-camera" size="large" />
+          </button>
+          <div className={styles.menu}>
+            <Flicking circular={false} bound>
+              {icons?.map((item, index) => (
+                <label key={index}>
+                  <input
+                    type="checkbox"
+                    className={styles.iconCheckbox}
+                    checked={motionAsset?.assetID == item.assetID}
+                    key={index}
+                    onChange={handleMotionChange(item)}
+                    style={{ position: "absolute", visibility: "hidden" }}
+                  />
+                  <div className={styles.iconBox}>
+                    <img src={item.assetImg} alt="" />
+                  </div>
+                </label>
+              ))}
+            </Flicking>
+          </div>
+        </div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
